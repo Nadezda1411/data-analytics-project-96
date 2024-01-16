af952473e770f898996a9de9dbb09fae378af7f9
Nadezda1411 <139394827+Nadezda1411@users.noreply.github.com>
2024-01-16 15:31:30 +0300

    Update last_paid_click.sql

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -1,4 +1,3 @@
-
 with table1 as (
     select distinct on (s.visitor_id)
         s.visitor_id,
@@ -11,13 +10,14 @@ with table1 as (
         l.amount,
         l.closing_reason,
         l.status_id
-    from sessions s
-    left join leads l
-        on s.visitor_id = l.visitor_id
-        and l.created_at >= s.visit_date
+    from sessions as s
+    left join leads as l
+        on
+            s.visitor_id = l.visitor_id
+            and s.visit_date <= l.created_at
     where s.medium in ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
-    order by 1, 2 desc)
+    order by 1, 2 desc
+)
 
 select * from table1
 order by 8 desc nulls last, 2, 3, 4, 5;
-
